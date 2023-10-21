857b33518cd57d6de455498c04d9654d0c428908
Rohan <rohandute1@gmail.com>
2023-10-22 01:09:55 +0530

    [rohan] added:- successfully calculated length of line.

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,8 +1,24 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
 public class LineComparison {
     public static void main(String[] args)
     {
         System.out.println("Welcome to Line Comparison Practice Problems!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter co-ordinates of x : ");
+        double x1 = sc.nextDouble();
+        double x2 = sc.nextDouble();
+
+        System.out.println("Enter co-ordinates of y : ");
+        double y1 = sc.nextDouble();
+        double y2 = sc.nextDouble();
+
+        double lengthOfLine = sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        System.out.println("Length of line is :"+lengthOfLine);
     }
 }
