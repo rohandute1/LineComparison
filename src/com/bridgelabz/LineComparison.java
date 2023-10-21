5ff7791e8856ca57a6d8a0bfbc1a5d32b25bf12c
Rohan <rohandute1@gmail.com>
2023-10-22 01:15:31 +0530

    [rohan] added:- successfully compared two line

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 import static java.lang.Math.sqrt;
@@ -10,15 +11,32 @@ public class LineComparison {
         System.out.println("Welcome to Line Comparison Practice Problems!");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter co-ordinates of x : ");
+        System.out.println("Enter co-ordinates of line 1 : ");
         double x1 = sc.nextDouble();
         double x2 = sc.nextDouble();
-
-        System.out.println("Enter co-ordinates of y : ");
         double y1 = sc.nextDouble();
         double y2 = sc.nextDouble();
 
-        double lengthOfLine = sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        System.out.println("Length of line is :"+lengthOfLine);
+        System.out.println("Enter co-ordinates of line 2 : ");
+        double x3 = sc.nextDouble();
+        double x4 = sc.nextDouble();
+        double y3 = sc.nextDouble();
+        double y4 = sc.nextDouble();
+
+        double lengthOfLine1 = sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        double lengthOfLine2 = sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2));
+
+        System.out.println("Length of line is :"+lengthOfLine1);
+        System.out.println("Length of line is :"+lengthOfLine2);
+
+        if(Objects.equals(lengthOfLine1, lengthOfLine2)){
+            System.out.println("Line 1 is equal to Line 2");
+        }
+        else if(lengthOfLine1>lengthOfLine2){
+            System.out.println("Line 1 is greater than Line 2");
+        }
+        else{
+            System.out.println("Line 2 is greater than Line1");
+        }
     }
 }
