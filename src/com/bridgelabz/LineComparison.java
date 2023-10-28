c9216f7f32c4c99425492d7edbe572fda006cb29
Rohan <rohandute1@gmail.com>
2023-10-28 15:09:21 +0530

    [rohan] added:- refactored code to adhere to OOP's principles.

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,43 +1,76 @@
 package com.bridgelabz;
 
-import java.util.Comparator;
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.getX(), 2) + Math.pow(this.y - other.getY(), 2));
+    }
+}
+
+class Line implements Comparable<Line> {
+    private Point startPoint;
+    private Point endPoint;
+
+    public Line(Point startPoint, Point endPoint) {
+        this.startPoint = startPoint;
+        this.endPoint = endPoint;
+    }
+
+    public double length() {
+        return startPoint.distance(endPoint);
+    }
+
+    @Override
+    public int compareTo(Line other) {
+        double thisLength = this.length();
+        double otherLength = other.length();
+        return Double.compare(thisLength, otherLength);
+    }
+}
 
 public class LineComparison {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Practice Problems!");
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter co-ordinates of line 1 : ");
-        double x1 = sc.nextDouble();
-        double x2 = sc.nextDouble();
-        double y1 = sc.nextDouble();
-        double y2 = sc.nextDouble();
+        System.out.println("Enter coordinates of line 1:");
+        Point point1 = new Point(sc.nextDouble(), sc.nextDouble());
+        Point point2 = new Point(sc.nextDouble(), sc.nextDouble());
+        Line line1 = new Line(point1, point2);
 
-        System.out.println("Enter co-ordinates of line 2 : ");
-        double x3 = sc.nextDouble();
-        double x4 = sc.nextDouble();
-        double y3 = sc.nextDouble();
-        double y4 = sc.nextDouble();
+        System.out.println("Enter coordinates of line 2:");
+        Point point3 = new Point(sc.nextDouble(), sc.nextDouble());
+        Point point4 = new Point(sc.nextDouble(), sc.nextDouble());
+        Line line2 = new Line(point3, point4);
 
-        double lengthOfLine1 = sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
-        double lengthOfLine2 = sqrt(Math.pow(x4-x3,2)+Math.pow(y4-y3,2));
+        System.out.println("Length of line 1: " + line1.length());
+        System.out.println("Length of line 2: " + line2.length());
 
-        System.out.println("Length of line is 1 :"+lengthOfLine1);
-        System.out.println("Length of line is 2 :"+lengthOfLine2);
-
-        int result = Double.compare(lengthOfLine1,lengthOfLine2);
-        if (result==0){
+        int result = line1.compareTo(line2);
+        if (result == 0) {
             System.out.println("Two lines are equal");
-        }
-        else if (result >0){
+        } else if (result > 0) {
             System.out.println("Line 1 is greater than Line 2");
-        }
-        else{
+        } else {
             System.out.println("Line 2 is greater");
         }
     }
 }
+
